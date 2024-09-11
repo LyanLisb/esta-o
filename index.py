1f19cd33261ba5133899a49e44d78557c956c07e
LyanLisb <lyan240406@gmail.com>
2024-09-11 16:33:30 -0300

    1

--- index.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config import config_all
-
-app = Flask(__name__)
-
-config_all(app)
-
-
--- routes/listas.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, request
-
-
-dados_route = Blueprint('dados',__name__)
-
-@dados_route.route('/')
-def tabela_geral ():
-    return 
